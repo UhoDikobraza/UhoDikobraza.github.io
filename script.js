00f3a9227799217b14cab0709f4165739ce9241d
UhoDikobraza <49452612+UhoDikobraza@users.noreply.github.com>
2019-04-19 01:23:11 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var listElement = document.getElementsByClassName("nvPanel");
+
+var mouseon = function(event){
+	event.target.style.boxShadow = "4px 6px 10px 4px #aaaaaa";
+};
+
+var mouseoff = function(event){
+	event.target.style.boxShadow = null;
+};
+
+for(elem of listElement){
+	elem.onmouseover = mouseon;
+};
+
+for(elem of listElement){
+	elem.onmouseout = mouseoff;
+};
+
+console.log(listElement);
