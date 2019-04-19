a2d08bf4c49576db12b9491adda390a9938ea7a6
UhoDikobraza <49452612+UhoDikobraza@users.noreply.github.com>
2019-04-19 22:02:53 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ var listElement = document.getElementsByClassName("nvPanel");
 
 var mouseon = function(event){
 	event.target.style.boxShadow = "4px 6px 10px 4px #aaaaaa";
+	event.target.style.fontWeight = "bold";
 };
 
 var mouseoff = function(event){
 	event.target.style.boxShadow = null;
+	event.target.style.fontWeight = null;
 };
 
 for(elem of listElement){
